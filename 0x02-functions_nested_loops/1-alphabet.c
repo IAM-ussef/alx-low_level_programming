8c023b36d56363089f7f02f508209b7d8cf5ed93
Youssef <you@example.com>
2023-07-18 08:06:16 -0700

    renaming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ * return - void
+*/
+void	print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
+/*int main(void)
+{
+    print_alphabet();
+    return (0);
+}*/
